de307a67c644284e59582b7f667d17d1cf99eba1
Kwok Lung Fan <fkt8356@gmail.com>
2020-07-23 22:29:15 -0400

    add custom time profile

--- mla/mla/timing.py
+++ mla/mla/timing.py
@@ -123,3 +123,69 @@ class gauss_profile(generic_profile):
         r""" Return the min/max values for the distribution 
         """
         return [-np.inf, np.inf]
+        
+        
+        
+class custom_profile(generic_profile):
+    r"""Time profile class for a user-defined pdf. Notice that the pdf have to be normalized. The effective_exposure is chosen such that the peak of the profile is 1.
+    """
+    def __init__(self, time_pdf , range , grid = None , size = 10000):
+        r""" Constructor for the class.
+
+        args:
+            time_pdf : the normalized pdf
+            range : the time range of the pdf(have to be finite)
+            grid : the time points that will be evaluated(overide size args)
+            size : the number of time points that will be evaluated with linear spacing
+        """
+        self.pdf = time_pdf
+        self.range = range
+        if grid is None:
+            self.grid = np.linspace(range[0],range[1], size) #grid size 
+        else :
+            self.grid = grid
+        self.dist = self.build_rv()
+        return
+    
+    def build_rv(self):
+        r""" build the distribution using scipy
+        """
+        hist = self.pdf(self.grid[:-1])
+        peak = self.grid[np.argmax(hist)]
+        self.norm = 1/peak
+        return scipy.stats.rv_histogram(hist,self.grid)
+    
+    def pdf(self, times):
+        r""" Calculates the probability for each time
+            
+        args:
+            times: A numpy list of times to evaluate
+        """
+        return self.pdf(times)
+    
+    def logpdf(self, times):
+        r""" Calculates the log(probability) for each time
+            
+        args:
+            times: A numpy list of times to evaluate
+        """
+        return np.log(self.pdf(times))
+        
+    def random(self, n=1): 
+        r""" Return random values following the custom distribution
+        
+        args:
+            n: The number of random values to return
+        """
+        return self.dist.rvs(size=n)
+    
+    def effective_exposure(self): 
+        r""" Calculate the weight associated with each
+            event time. 
+        """
+        return 1.0/self.norm
+    
+    def get_range(self): 
+        r""" Return the min/max values for the distribution 
+        """
+        return [self.range[0], self.range[1]]
