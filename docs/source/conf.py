fd4fd5875f1244c4d40ae8d435e58edceaf2ceda
thejevans <thejevans@gmail.com>
2020-09-29 16:16:36 -0400

    updated typehint documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,11 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx.ext.napoleon'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
