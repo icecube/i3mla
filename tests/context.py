543f9f0cc984154e7b87c684a5ffed369f075a4f
thejevans <thejevans@gmail.com>
2020-12-14 01:30:42 -0500

    added header boilerplate

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,14 @@
 """Appends the repository directory to the python path for running tests."""
 
+__author__ = 'John Evans'
+__copyright__ = 'Copyright 2020 John Evans'
+__credits__ = ['John Evans', 'Jason Fan', 'Michael Larson']
+__license__ = 'Apache License 2.0'
+__version__ = '0.0.1'
+__maintainer__ = 'John Evans'
+__email__ = 'john.evans@icecube.wisc.edu'
+__status__ = 'Development'
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
