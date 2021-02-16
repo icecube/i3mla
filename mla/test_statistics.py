2c099d3dd6df84fd7bb7892d277ab7588157cae2
Kwok Lung Fan <fkt8356@gmail.com>
2021-02-16 13:20:26 -0500

    lint

--- mla/test_statistics.py
+++ mla/test_statistics.py
@@ -130,6 +130,8 @@ def _i3_ts(sob: np.ndarray, prepro: Preprocessing,
     if prepro.n_events == 0:
         return 0
 
+    ns_ratio = _calculate_ns_ratio(sob)
+
     if return_ns:
         return ns_ratio * prepro.n_events
 
