13a00e90c8207d164f140e175b3cc6f3a4e759f1
Kwok Lung(Jason) Fan <fkt8356@gmail.com>
2022-11-28 10:34:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="mla",
-    version="1.3.0",
+    version="1.4.0",
     author="John Evans, Jason Fan, Michael Larson",
     author_email="john.evans@icecube.wisc.edu",
     description="IceCube analysis tools for use with public data",
